629d903b24746d4f1018b77a151f48bc1f8523a4
Azizbek201798 <ziyodulayeva9@gmail.com>
2024-08-16 20:32:32 +0500

    HW

--- index.php
+++ index.php
@@ -1,16 +1,21 @@
 <?php
 
 declare(strict_types=1);
-
 require_once __DIR__ . '/vendor/autoload.php';
-
 require 'bootstrap.php';
 
 use App\User;
+use App\Ads;
 
 $user = new User();
+$ads = new Ads();
 
 // $user->create(1,'Azizbek','Chief','male','+99891 005 8110');
 // $user->deleteUser(1);
 // $user->updateUser(1,"Bekki","Player","male","+99890 100 1010");
 // var_dump($user->getUser(1));
+
+// $ads->createAds(1,'Novostroyka','Yangi',1,1,'1','1','1','1');
+// $ads->deleteAds(1);
+// $ads->updateAds(2,10,'Novostroyka','Yangi','1',1,'1',1,1,'1');
+// var_dump($ads->getAds(2));
--- src/Ads.php
+++ src/Ads.php
@@ -1,13 +1,9 @@
 <?php
 
 namespace App;
-
 use PDO;
-
-class User{
-
+class Ads{
     private PDO $pdo;
-
     public function __construct(){
         $this->pdo = DB::connect();
     }
@@ -16,8 +12,8 @@ class User{
         int $user_id,
         string $title,
         string $describtion,
-        string $status_id,
-        string $branch_id,
+        int $status_id,
+        int $branch_id,
         string $address,
         string $price,
         string $rooms,
@@ -25,10 +21,10 @@ class User{
     )
     {
         $query = "INSERT INTO ads(user_id,title,describtion,status_id,branch_id,address,price,rooms,branch,created_at) 
-                  VALUES (:user_id,:title,:describtion,:status_id,:branch_id,address,price,rooms,branch,NOW());";
+                  VALUES (:user_id,:title,:describtion,:status_id,:branch_id,:address,:price,:rooms,:branch,NOW());";
         $stmt = $this->pdo->prepare($query);
-        $stmt->bindParam(":id",$user_id);
-        $stmt->bindParam(":titlr",$title);
+        $stmt->bindParam(":user_id",$user_id);
+        $stmt->bindParam(":title",$title);
         $stmt->bindParam(":describtion",$describtion);
         $stmt->bindParam(":status_id",$status_id);
         $stmt->bindParam(":branch_id",$branch_id);
@@ -49,15 +45,21 @@ class User{
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
-    public function updateAds(int $id,string $username,string $position,string $gender,string $phone){
-        $query =  "UPDATE ads SET user_id = :user_id, title = :title, describtion = :describtion, status_id = :status_id, branch_id = :branch_id, address = :address, price = :price, rooms = :rooms, branch = :branch 
+    public function updateAds(int $id,int $user_id,string $title,string $describtion,string $status_id,int $branch_id,string $address,float $price, int $rooms, string $branch){
+        $query =  "UPDATE ads 
+                   SET user_id = :user_id, title = :title, describtion = :describtion, status_id = :status_id, branch_id = :branch_id, address = :address, price = :price, rooms = :rooms, branch = :branch 
                    WHERE id = :id;";
         $stmt = $this->pdo->prepare($query);
         $stmt->bindParam(":id",$id);
-        $stmt->bindParam(":username",$username);
-        $stmt->bindParam(":position",$position);
-        $stmt->bindParam(":gender",$gender);
-        $stmt->bindParam(":phone",$phone);
+        $stmt->bindParam(":user_id",$user_id);
+        $stmt->bindParam(":title",$title);
+        $stmt->bindParam(":describtion",$describtion);
+        $stmt->bindParam(":status_id",$status_id);
+        $stmt->bindParam(":branch_id",$branch_id);
+        $stmt->bindParam(":address",$address);
+        $stmt->bindParam(":price",$price);
+        $stmt->bindParam(":rooms",$rooms);
+        $stmt->bindParam(":branch",$branch);
         $stmt->execute();
     }
 
