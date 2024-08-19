ddb400aa2b412d2ee151368d00d27f209bd75f02
Azizbek201798 <ziyodulayeva9@gmail.com>
2024-08-19 20:56:48 +0500

    HW

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 use App\Router;
 
 Router::get('/', fn()=>loadController('home'));
-
 Router::get('/ads/{id}', function (int $id){
    loadController('showAd', ['id' => $id]);
 });
+Router::get('/ads/create',fn() => loadView('admin/create_ad'));
+Router::post('/ads/create',fn() => loadController('createAd'));
