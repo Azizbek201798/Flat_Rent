cec9ea0c8dd16b568fd47cfed9131842e76e5a78
Azizbek201798 <ziyodulayeva9@gmail.com>
2024-08-19 14:30:00 +0500

    HW

--- public/pages/single-ad.php
+++ public/pages/single-ad.php
@@ -1,17 +1,10 @@
 <?php
-/** @var TYPE_NAME $ad */
-/**
- * Credits:
- * https://tailwindflex.com/@arya/responsive-products-grid
- * source: https://github.com/mfg888/Responsive-Tailwind-CSS-Grid/blob/main/index.html
- */
 ?>
 <div class="text-center p-10">
     <h1 class="font-bold text-4xl mb-4">Flats for rent</h1>
 </div>
 <section id="Projects"
          class="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
-    <!--   ✅ Product card 1 - Starts Here 👇 -->
     <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
         <a href="/ads/<?= $ad->id ?>">
             <img src="https://images.unsplash.com/photo-1646753522408-077ef9839300?ixlib=rb-1.2.1&ixid=MnwxMjA"
@@ -45,5 +38,4 @@
             </div>
         </a>
     </div>
-    <!--   🛑 Product card 1 - Ends Here  -->
 </section>
